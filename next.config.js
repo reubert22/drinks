62dd9532ce84f5c02373103cfcb4fcf1cc73784c
Reubert Barbosa <reubertfernandes@gmail.com>
2021-11-30 20:40:22 -0300

    Adding offline page pwa

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 const withPWA = require("next-pwa");
+const runtimeCaching = require("next-pwa/cache");
 
 module.exports = withPWA({
   reactStrictMode: true,
@@ -6,6 +7,8 @@ module.exports = withPWA({
     dest: "public",
     register: true,
     skipWaiting: true,
+    runtimeCaching,
     disable: process.env.NODE_ENV === "development",
+    buildExcludes: [/middleware-manifest\.json$/],
   },
 });
