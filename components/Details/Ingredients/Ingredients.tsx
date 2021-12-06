a2fb31004e23f46b4b2049e1e15c70259658cec4
Reubert Barbosa <reubertfernandes@gmail.com>
2021-12-06 00:28:03 -0300

    imp: create style for details page

---
+++ components/Details/Ingredients/Ingredients.tsx
@@ -0,0 +1,19 @@
+export const Ingredients = ({ cocktail }) => (
+  <div id="details-ingredients">
+    <h1>Ingredients</h1>
+    {Array(15)
+      .fill("")
+      .map((_, id) => {
+        const ingredient = cocktail[`strIngredient${id + 1}`];
+        const measure = cocktail[`strMeasure${id + 1}`];
+
+        return (
+          !!ingredient && (
+            <p>
+              ({measure}): {ingredient}
+            </p>
+          )
+        );
+      })}
+  </div>
+);
--- pages/cocktail/[id]/details.js
+++ pages/cocktail/[id]/details.js
@@ -3,6 +3,16 @@ import { useRouter } from "next/router";
 import Image from "next/image";
 import { listDetails } from "../../../containers/cocktail/service.ts";
 import styles from "../../../styles/Details.module.scss";
+import { Ingredients } from "../../../components/Details/Ingredients/Ingredients.tsx";
+
+const DetailsFirstSection = ({ label, info, className }) => (
+  <div className={styles[className]}>
+    <span>
+      {label || ""}
+      {info || ""}
+    </span>
+  </div>
+);
 
 const Details = () => {
   const router = useRouter();
@@ -17,6 +27,14 @@ const Details = () => {
     };
     if (id) getDetails();
   }, [id]);
+
+  const handleScroll = () => {
+    document.getElementById("details-ingredients").scrollIntoView({
+      behavior: "smooth",
+      block: "center",
+      inline: "center",
+    });
+  };
 
   return (
     <div className={styles.container}>
@@ -44,63 +62,36 @@ const Details = () => {
           </div>
           <div className={styles.cocktail_info}>
             <span className={styles.strDrink}>{cocktail.strDrink}</span>
-            <h3>Category: </h3>
-            <p>{cocktail.strCategory}</p>
-            <h3>Alcoholic: </h3>
-            <p>{cocktail.strAlcoholic}</p>
-            <h3>Glass: </h3>
-            <p>{cocktail.strGlass}</p>
-            <h3>Instructions: </h3>
-            <p>{cocktail.strInstructions}</p>
-
-            <div>
-              <h1>Ingredients</h1>
-              <p>
-                ({cocktail.strMeasure1}): {cocktail.strIngredient1}
-              </p>
-              <p>
-                ({cocktail.strMeasure2}): {cocktail.strIngredient2}
-              </p>
-              <p>
-                ({cocktail.strMeasure3}): {cocktail.strIngredient3}
-              </p>
-              <p>
-                ({cocktail.strMeasure4}): {cocktail.strIngredient4}
-              </p>
-              <p>
-                ({cocktail.strMeasure5}): {cocktail.strIngredient5}
-              </p>
-              <p>
-                ({cocktail.strMeasure6}): {cocktail.strIngredient6}
-              </p>
-              <p>
-                ({cocktail.strMeasure7}): {cocktail.strIngredient7}
-              </p>
-              <p>
-                ({cocktail.strMeasure8}): {cocktail.strIngredient8}
-              </p>
-              <p>
-                ({cocktail.strMeasure9}): {cocktail.strIngredient9}
-              </p>
-              <p>
-                ({cocktail.strMeasure10}): {cocktail.strIngredient10}
-              </p>
-              <p>
-                ({cocktail.strMeasure11}): {cocktail.strIngredient11}
-              </p>
-              <p>
-                ({cocktail.strMeasure12}): {cocktail.strIngredient12}
-              </p>
-              <p>
-                ({cocktail.strMeasure13}): {cocktail.strIngredient13}
-              </p>
-              <p>
-                ({cocktail.strMeasure14}): {cocktail.strIngredient14}
-              </p>
-              <p>
-                ({cocktail.strMeasure15}): {cocktail.strIngredient15}
-              </p>
+            <span className={styles.alcoholic_flag}>
+              {cocktail.strAlcoholic}
+            </span>
+            <div className={styles.last_section}>
+              {!!cocktail.strInstructions && (
+                <DetailsFirstSection
+                  info={cocktail.strInstructions}
+                  className="instructions"
+                />
+              )}
+              {!!cocktail.strCategory && (
+                <DetailsFirstSection
+                  label="Category: "
+                  info={cocktail.strCategory}
+                  className="category"
+                />
+              )}
+              {!!cocktail.strGlass && (
+                <DetailsFirstSection
+                  label="Glass: "
+                  info={cocktail.strGlass}
+                  className="glass"
+                />
+              )}
+              {/* <button onClick={handleScroll} className={styles.button}>
+                <span className={styles.arrow}>ꜜ</span>
+              </button> */}
             </div>
+
+            <Ingredients cocktail={cocktail} />
           </div>
         </div>
       ))}
